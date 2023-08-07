76f760a98f03a9b9aab15a0150bdf5db3bb209e6
AlexandraNovinskaya <alexandranovinskaya@gmail.com>
2023-08-07 17:29:13 +0300

    last ver

--- src/components/CardsItems/CardsItems.js
+++ src/components/CardsItems/CardsItems.js
@@ -1,131 +1,177 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
+import './CardsItems.css';
 import { Pagination, Alert } from 'antd';
 import MovieService from '../../services/MovieService';
 import SearchComponent from '../SearchComponent/SearchComponent';
 import Spinner from '../Spinner/Spinner';
 import { Rate } from 'antd';
 import RatedTab from '../RatedTab/RatedTab';
-import { useRatingContext } from '../RatingContext/RatingContext';
-import "./CardsItems.css"
+import { useGenreContext } from '../GenreContext/GenreContext';
 
-function CardsItems(props) {
-    const [loading, setLoading] = useState(true);
-    const [movies, setMovies] = useState([]);
-    const [noResults, setNoResults] = useState(false);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [pageSize, setPageSize] = useState(6);
-    const [totalResults, setTotalResults] = useState(0);
-    const [error, setError] = useState(null);
-    const [page, setPage] = useState(1);
-    const [guestSessionId, setGuestSessionId] = useState(null);
 
-    const { addRatedMovie } = useRatingContext(); // Получение функции addRatedMovie из контекста оценок
 
-    const movieService = new MovieService();
+// import { MovieServiceProvider } from '../MovieServiceCont/MovieServiceCont';
 
-    //форматирование даты 
-    const formatDate = (inputDate) => {
-        const date = new Date(inputDate);
-        const option = { year: 'numeric', month: 'long', day: 'numeric' };
-        return date.toLocaleDateString('en-US', option);
+function formatDate(inputDate) {
+    const date = new Date(inputDate);
+    const option = { year: 'numeric', month: 'long', day: 'numeric' };
+    return date.toLocaleDateString('en-US', option);
+}
+
+function lessText(text, maxLength) {
+    if (text.length > maxLength) {
+        return text.substring(0, maxLength - 3) + '...';
+    }
+    return text;
+}
+
+function getRatingColor(rating) {
+    if (rating >= 0 && rating < 3) {
+        return '#E90000';
+    } else if (rating >= 3 && rating < 5) {
+        return '#E97E00';
+    } else if (rating >= 5 && rating < 7) {
+        return '#E9D100';
+    } else if (rating >= 7) {
+        return '#66E900';
+    }
+}
+
+export default class CardsItems extends Component {
+    movieService = new MovieService();
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: true,
+            movies: [],
+            noResults: false,
+            currentPage: 1,
+            pageSize: 6,
+            totalResults: 0,
+            error: null,
+            page: 1,
+            guestSessionId: null,
+        };
+
+
     }
 
-    //сокращение текста 
-    const lessText = (text, maxLength) => {
-        if (text.length > maxLength) {
-            return text.substring(0, maxLength - 3) + '...';
-        }
-        return text;
+
+
+    componentDidMount() {
+        this.movieService.createGuestSession()
+            .then((guestSessionId) => {
+                this.setState({
+                    guestSessionId: guestSessionId
+                });
+
+                const { page } = this.state;
+                this.updateMovies(page);
+            })
+            .catch((error) => {
+                console.error("Failed to create guest session:", error);
+            });
     }
 
-    //выставление цвета 
-    const getRatingColor = (rating) => {
-        if (rating >= 0 && rating < 3) {
-            return '#E90000';
-        } else if (rating >= 3 && rating < 5) {
-            return '#E97E00';
-        } else if (rating >= 5 && rating < 7) {
-            return '#E9D100';
-        } else if (rating >= 7) {
-            return '#66E900';
-        }
-    }
 
-    const updateMovies = (page) => {
-        setLoading(true);
-        setNoResults(false);
-        setError(null);
+    updateMovies(page) {
+        const { pageSize } = this.state;
+
+        this.setState({ loading: true, noResults: false, error: null });
+        // console.log(page)
 
         const searchQuery = '';
 
-        movieService.getMovieByPage(searchQuery, page, pageSize)
+        this.movieService
+            .getMovieByPage(searchQuery, page, pageSize)
+            // .getMovie('', pageSize, page)
             .then((results) => {
                 if (results.length === 0) {
-                    setLoading(false);
-                    setMovies([]);
-                    setNoResults(true);
+                    this.setState({
+                        loading: false,
+                        movies: [],
+                        noResults: true,
+                    });
                 } else {
-                    setLoading(false);
-                    setMovies(results);
-                    setNoResults(false);
-                    setTotalResults(results.totalResults);
-                    setCurrentPage(page);
+                    this.setState({
+                        loading: false,
+                        movies: results,
+                        noResults: false,
+                        totalResults: results.totalResults,
+                        currentPage: page,
+                    });
                 }
             })
             .catch((error) => {
-                setLoading(false);
-                setMovies([]);
-                setNoResults(false);
-                setError(error.message);
+                this.setState({
+                    loading: false,
+                    movies: [],
+                    noResults: false,
+                    error: error.message,
+                });
             });
     }
 
-    const updateMoviesWithSearch = (searchQuery) => {
-        setLoading(true);
-        setNoResults(false);
-        setError(null);
 
-        movieService.getMovieByPage(searchQuery, page, pageSize)
+    updateMoviesWithSearch = (searchQuery) => {
+        this.setState({ loading: true, noResults: false, error: null });
+
+        const { page, pageSize } = this.state
+
+        this.movieService
+            .getMovieByPage(searchQuery, page, pageSize)
+            // .getMovie(searchQuery)
             .then((results) => {
                 if (results.length === 0) {
-                    setLoading(false);
-                    setMovies([]);
-                    setNoResults(true);
+                    this.setState({
+                        loading: false,
+                        movies: [],
+                        noResults: true,
+                    });
                 } else {
-                    setLoading(false);
-                    setMovies(results);
-                    setNoResults(false);
+                    this.setState({
+                        loading: false,
+                        movies: results,
+                        noResults: false,
+                    });
                 }
             })
             .catch((error) => {
-                setLoading(false);
-                setMovies([]);
-                setNoResults(false);
-                setError(error.message);
+                this.setState({
+                    loading: false,
+                    movies: [],
+                    noResults: false,
+                    error: error.message,
+                });
             });
     }
 
-    const handlePageChange = (page) => {
-        updateMovies(page);
+    handlePageChange = (page) => {
+        this.updateMovies(page);
+        // console.log(page)
     }
 
-    const handleRatingChange = (movieId, value) => {
-        movieService.rateMovie(movieId, value)
+    handleRatingChange = (movieId, value) => {
+        // Отправьте оценку на сервер с использованием вашего MovieService
+        this.movieService.rateMovie(movieId, value)
             .then(() => {
-                const updatedMovies = movies.map((movie) => {
+                // Обновите состояние фильма с новой оценкой
+                const updatedMovies = this.state.movies.map((movie) => {
                     if (movie.id === movieId) {
                         return { ...movie, rating: value };
                     }
                     return movie;
                 });
 
+                // const ratedMovie = {
+                //     id: movieId,
+                //     title: movie.title,
+                //     rating: value
+                // }
 
-                setMovies(updatedMovies, () => {
-                    addRatedMovie({
-                        id: movieId,
-                        title: updatedMovies.find(movie => movie.id === movieId).title,
-                        rating: value
-                    });
+                this.setState({ movies: updatedMovies }, () => {
+                    this.ratedTabRef.addRatedMovie({ id: movieId, title: updatedMovies.find(movie => movie.id === movieId).title, rating: value });
                 });
             })
             .catch((error) => {
@@ -133,67 +179,69 @@ function CardsItems(props) {
             });
     }
 
-    useEffect(() => {
-        movieService.createGuestSession()
-            .then((guestSessionId) => {
-                setGuestSessionId(guestSessionId);
-                updateMovies(page);
-            })
-            .catch((error) => {
-                console.error("Failed to create guest session:", error);
-            });
-    }, []);
 
-    return (
-        <div className='wind-size'>
-            <div className='search-line'>
-                <SearchComponent onSearch={updateMoviesWithSearch} />
-            </div>
-            {loading ? (
-                <Spinner />
-            ) : noResults ? (
-                <div>No results found.</div>
-            ) : error ? (
-                <Alert message="Error" description={error} type="error" />
-            ) : (
-                <div className='movie-col'>
-                    {movies.map((movie) => (
-                        <div key={movie.id} className='item-contener'>
-                            <div className='rating-circle' style={{ backgroundColor: getRatingColor(movie.rating) }}>
-                                {movie.rating}
-                            </div>
-                            <img
-                                className='movie-poster'
-                                src={`https://www.themoviedb.org/t/p/original/${movie.poster_path}`}
-                                alt={movie.title}
-                            />
-                            <div>
-                                <h5 className='mov-title'>{movie.title}</h5>
-                                <p className='release_date'>{formatDate(movie.release_date)}</p>
-                                <p className='overview'>{lessText(movie.overview, 150)}</p>
-                                <Rate
-                                    className='stars'
-                                    allowHalf
-                                    value={movie.rating}
-                                    onChange={(value) => handleRatingChange(movie.id, value)}
-                                />
-                                <RatedTab />
-                            </div>
-                        </div>
-                    ))}
-                    <div className="pagination-container">
-                        <Pagination
-                            current={currentPage}
-                            pageSize={pageSize}
-                            total={Math.min(totalResults, 20)}
-                            onChange={handlePageChange}
-                            className='pagination'
-                        />
-                    </div>
+    render() {
+        const { loading, movies, noResults, currentPage, pageSize, totalResults, error } = this.state;
+
+        // const { getGenreById } = useGenreC?ontext();
+        return (
+            // <GenreContext.Consumer>
+
+            <div className='wind-size'>
+                <div className='search-line'>
+                    <SearchComponent onSearch={this.updateMoviesWithSearch} />
                 </div>
-            )}
-        </div>
-    );
+
+
+                {loading ? (
+                    <Spinner />
+                ) : noResults ? (
+                    <div>No results found.</div>
+                ) : error ? (
+                    <Alert message="Error" description={error} type="error" />
+                ) : (
+                    <div className='movie-col'>
+                        {movies.map((movie) => (
+                            <div key={movie.id} className='item-contener'>
+                                <div className='rating-circle' style={{ backgroundColor: getRatingColor(movie.rating) }}>
+                                    {movie.rating}
+                                </div>
+                                <img
+                                    className='movie-poster'
+                                    src={`https://www.themoviedb.org/t/p/original/${movie.poster_path}`}
+                                    alt={movie.title}
+                                />
+                                <div>
+                                    <h5 className='mov-title'>{movie.title}</h5>
+                                    {/* <p>{genres?}</p> */}
+                                    <p className='release_date'>{formatDate(movie.release_date)}</p>
+                                    <p className='overview'>{lessText(movie.overview, 250)}</p>
+                                    <Rate
+                                        allowHalf
+                                        value={movie.rating}
+                                        onChange={this.handleRatingChange.bind(this, movie.id)}
+                                    />
+                                    <RatedTab />
+                                </div>
+
+                            </div>
+                        ))}
+                        <div className="pagination-container">
+                            <Pagination
+                                current={currentPage}
+                                pageSize={pageSize}
+                                total={(Math.min(totalResults, 20))}
+                                onChange={this.handlePageChange}
+                                className='pagination'
+                            />
+                        </div>
+                    </div>
+                )}
+            </div>
+        )
+    }
+
+
 }
 
-export default CardsItems;
+
